2dfc0841a0169ca2f042e9606917d0d862fa3234
cheonjiwan <giyan1000@naver.com>
2020-08-26 15:10:00 +0900

    db connection

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var io = require('socket.io');
+var path = require('path');
 var app = express();
 
+app.use(express.static(path.join(__dirname, 'html')));
 app.get("/", function(req, res){
-    res.send("Hello world!");
-    res.send("seongwoo let's go!");
+    res.sendFile(path.join(__dirname, 'html', 'main.html'));
 });
 
 
@@ -22,7 +23,44 @@ app.get("/gps", function(req, res){
     res.send(JSON.stringify(obj));
 });
 
+app.get("/about", function (req, res) {
+    res.sendFile(path.join(__dirname, 'html', 'about.html'));
+    
+    var mysql = require("mysql"); // mysql 모듈을 불러옵니다.
 
+    // 커넥션을 정의합니다.
+    // RDS Console 에서 본인이 설정한 값을 입력해주세요.
+    var connection = mysql.createConnection({
+    host: "userinfo-db.cfndrgetutvs.ap-northeast-2.rds.amazonaws.com",
+    user: "jiwan",
+    password: "wan2good",
+    database: "UserInfo"
+    });
+
+    var device_id = req.query.id;
+    var user_name = req.query.name;
+    var age = req.query.age;
+    var sex = req.query.gender;
+    var phone_num = req.query.phonenumber;
+    var photo_url = req.query.photo;
+    var user_address = req.query.address;
+    var etc = req.query.etc;
+
+    var sql = "insert into user_info values ("+device_id+","+user_name+","+age+","+sex+","+phone_num+","+photo_url+","+user_address+","+etc+")"
+    var selectsql = "select * from user_info"
+    // RDS에 접속합니다.
+    connection.connect(function(err) {
+        if (err) {
+            throw err; // 접속에 실패하면 에러를 throw 합니다.
+        } else {
+            // 접속시 쿼리를 보냅니다.
+            connection.query(selectsql, function(err, rows, fields) {
+                console.log(rows); // 결과를 출력합니다!
+            });
+        }
+    });
+    
+});
 
 var port = process.env.PORT || 8081;
 var server = app.listen(port, function(){
